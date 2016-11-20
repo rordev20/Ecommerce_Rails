0656311275a8ec98347864102bd0455f76ed7441
rordev20 <rordev20@gmail.com>
2016-11-20 15:49:33 +0530

    added show action and its assets and proper indentation for listing page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
+    @product = Product.all
+  end
+
+  def show
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   root :to => "products#index"
 end
