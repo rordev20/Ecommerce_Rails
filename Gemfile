a7776620ecf6d33b1a5965786c92ca598b460f2b
rordev20 <rordev20@gmail.com>
2017-03-31 22:36:15 +0530

    added friendly id gem

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'redis'
 gem 'redis-namespace'
 gem 'redis-rails'
 gem 'redis-rack-cache'
+gem 'friendly_id', '~> 5.1.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120075313) do
+ActiveRecord::Schema.define(version: 20170331133557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -192,6 +192,19 @@ ActiveRecord::Schema.define(version: 20161120075313) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
   create_table "images", force: :cascade do |t|
     t.integer  "product_id"
     t.string   "photo"
@@ -290,6 +303,7 @@ ActiveRecord::Schema.define(version: 20161120075313) do
     t.integer  "discount_percent"
     t.float    "discount_amount"
     t.boolean  "in_stock"
+    t.string   "slug"
     t.boolean  "is_active"
     t.datetime "deleted_at"
     t.datetime "created_at",        null: false
@@ -299,6 +313,7 @@ ActiveRecord::Schema.define(version: 20161120075313) do
   add_index "products", ["color"], name: "index_products_on_color", using: :btree
   add_index "products", ["name"], name: "index_products_on_name", using: :btree
   add_index "products", ["sell_price"], name: "index_products_on_sell_price", using: :btree
+  add_index "products", ["slug"], name: "index_products_on_slug", unique: true, using: :btree
   add_index "products", ["sub_category_id"], name: "index_products_on_sub_category_id", using: :btree
   add_index "products", ["vendor_id"], name: "index_products_on_vendor_id", using: :btree
 
