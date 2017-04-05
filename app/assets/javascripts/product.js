66ee5f48d5a4109a543e5433946958b4cd2dc225
rordev20 <rordev20@gmail.com>
2017-04-05 23:34:32 +0530

    removed slick js and css file and added spinner

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -2,7 +2,7 @@ var ProductUtil = {}
 
 $(document).ready(function () {
     //initiate the plugin and pass the id of the div containing gallery images
-  $("#cover_image").elevateZoom({gallery:'image_gallery', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true, loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'}); 
+  $("#cover_image").elevateZoom({gallery:'image_gallery', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true, loadingIcon: '/assets/spinner.gif'});
 
   //pass the images to Fancybox
   $("#cover_image").bind("click", function(e) {
