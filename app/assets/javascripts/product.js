d48d900184ed9f47753f279460a9c772eae3778c
rordev20 <rordev20@gmail.com>
2017-04-05 23:21:32 +0530

    Added product image gallery and zoom feature

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,1 +1,13 @@
+var ProductUtil = {}
 
+$(document).ready(function () {
+    //initiate the plugin and pass the id of the div containing gallery images
+  $("#cover_image").elevateZoom({gallery:'image_gallery', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true, loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'}); 
+
+  //pass the images to Fancybox
+  $("#cover_image").bind("click", function(e) {
+    var ez =   $('#cover_image').data('elevateZoom');
+    $.fancybox(ez.getGalleryList());
+    return false;
+  });
+});
