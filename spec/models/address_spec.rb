a6e23005985f444ad4a61ad475266dd8a322de34
Rohit Patel <rohit.patel@infibeam.net>
2019-10-03 18:24:22 +0530

    address association and validation spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:state) }
+    it { should belong_to(:country) }
+    it { should belong_to(:vendor) }
+    it { should belong_to(:address_type) }
+  end
+
+  describe 'validations' do
+    subject { FactoryBot.build(:address) }
+    it { should validate_presence_of(:country) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:address1) }
+    it { should validate_presence_of(:zipcode) }
+    it { should validate_presence_of(:city) }
+  end
 end
